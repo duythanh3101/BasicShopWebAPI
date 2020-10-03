50cf182d5cbfc510074fc4d4156a1b2e5fd0ce14
duythanhpham3101 <duythanh3101@gmail.com>
2020-10-04 02:18:09 +0700

    Add entities and database context

---
+++ BasicShopWebAPI.Data/Configurations/CartConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using BasicShopWebAPI.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicShopWebAPI.Data.Configurations
+{
+    public class CartConfiguration : IEntityTypeConfiguration<Cart>
+    {
+        public void Configure(EntityTypeBuilder<Cart> builder)
+        {
+            builder.ToTable("Carts");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+
+            builder.HasOne(x => x.Product).WithMany(x => x.Carts).HasForeignKey(x => x.ProductId);
+
+        }
+    }
+}
---
+++ BasicShopWebAPI.Data/EF/EShopDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using BasicShopWebAPI.Data.EF;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BasicShopWebAPI.Data.EF
+{
+    public class EShopDbContextFactory : IDesignTimeDbContextFactory<EShopDbContext>
+    {
+        public EShopDbContext CreateDbContext(string[] args)
+        {
+            //IConfigurationRoot configuration = new ConfigurationBuilder()
+            //    .SetBasePath(Directory.GetCurrentDirectory())
+            //    .AddJsonFile("appsettings.json")
+            //    .Build();
+
+            //var connectionString = configuration.GetConnectionString("eShopSolutionDb");
+
+            //var optionsBuilder = new DbContextOptionsBuilder<EShopDbContext>();
+            //optionsBuilder.UseSqlServer(connectionString);
+
+            //return new EShopDbContext(optionsBuilder.Options);
+            return new EShopDbContext(null);
+        }
+    }
+}
